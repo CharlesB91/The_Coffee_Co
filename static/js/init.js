b38dabcaa14d2114f84226f189146611a78705f3
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2022-07-03 18:09:53 +0000

    base html template added with materialize css components

--- static/js/init.js
+++ static/js/init.js
@@ -7,6 +7,7 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-$(document).ready(function(){
-  $('.modal').modal();
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems, options);
 });
