b70fcbdda8ddd419f6eb9f80f321d6a3ec191ef4
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2022-07-03 18:29:18 +0000

    added account management options to nav

--- static/js/init.js
+++ static/js/init.js
@@ -7,7 +7,3 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.dropdown-trigger');
-  var instances = M.Dropdown.init(elems, options);
-});
