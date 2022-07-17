ff5499efa00379ce552e7291421a89a1f7d73429
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2022-07-17 20:19:28 +0000

    basic toasts now displaying when product added

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
 
@@ -10,7 +12,7 @@ def view_cart(request):
 
 def add_to_cart(request, item_id):
     """  """
-
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -19,6 +21,7 @@ def add_to_cart(request, item_id):
         cart[item_id] += quantity
     else:
         cart[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
