7b569193abf96b938aa1aabde3dac9c6cdd9a042
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2022-07-17 16:02:14 +0000

    items successfully deleting from cart

--- cart/views.py
+++ cart/views.py
@@ -3,12 +3,13 @@ from django.shortcuts import render, redirect, reverse, HttpResponse
 # Create your views here.
 
 def view_cart(request):
-    """ A view to return the index page """
+    """  """
 
     return render(request, 'cart/cart.html')
 
+
 def add_to_cart(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
+    """  """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -22,8 +23,9 @@ def add_to_cart(request, item_id):
     request.session['cart'] = cart
     return redirect(redirect_url)
 
+
 def edit_cart(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
+    """  """
 
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
@@ -35,3 +37,19 @@ def edit_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
+
+
+def remove_from_cart(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        cart = request.session.get('cart', {})
+        cart.pop(item_id)
+
+        request.session['cart'] = cart
+        return redirect(reverse('view_cart'))
+        return HttpResponse(status=200)
+        
+    except Exception as e:
+        return HttpResponse(status=500)
+
