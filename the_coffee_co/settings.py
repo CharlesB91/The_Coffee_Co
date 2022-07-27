efab3b1e50f37a4b269d43b32ac34c3fd7539e05
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2022-07-27 18:08:52 +0000

    deployment test

--- the_coffee_co/settings.py
+++ the_coffee_co/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-2=_lo8sny(ms_&lu+f^=!yh!no=+*(ya5*v*zo)-!27lh3--x)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['thecoffeeco.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -119,12 +119,17 @@ WSGI_APPLICATION = 'the_coffee_co.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 
