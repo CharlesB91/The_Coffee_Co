59106263890ab305843d3d14327da2b0d7e538a4
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2022-07-28 19:26:10 +0000

    styles changed made

--- the_coffee_co/settings.py
+++ the_coffee_co/settings.py
@@ -22,16 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
-
-# SECURITY WARNING: don't run with debug turned on in production!
+# # SECURITY WARNING: don't run with debug turned on in production!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 ALLOWED_HOSTS = ['thecoffeeco.herokuapp.com', 'localhost']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+
+# DEBUG = 'DEVELOPMENT' in os.environ
+
+DEBUG = True
 
 
 INSTALLED_APPS = [
@@ -132,7 +132,6 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
 
 
 # Password validation
