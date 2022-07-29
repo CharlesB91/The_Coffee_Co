2feb1bdb97f6e54f5394d4abda66c3a79e02c725
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2022-07-29 13:43:24 +0000

    js code readded

--- static/js/materialize.js
+++ static/js/materialize.js
@@ -525,14 +525,14 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
       return this[0].getBoundingClientRect()[lower];
     };
 
-  //   fn["inner" + v] = function () {
-  //     return this[0]["client" + v];
-  //   };
+    fn["inner" + v] = function () {
+      return this[0]["client" + v];
+    };
 
-  //   fn["outer" + v] = function (margins) {
-  //     return this[0]["offset" + v] + (margins ? compute(this, "margin" + (v === "Width" ? "Left" : "Top")) + compute(this, "margin" + (v === "Width" ? "Right" : "Bottom")) : 0);
-  //   };
-  // });
+    fn["outer" + v] = function (margins) {
+      return this[0]["offset" + v] + (margins ? compute(this, "margin" + (v === "Width" ? "Left" : "Top")) + compute(this, "margin" + (v === "Width" ? "Right" : "Bottom")) : 0);
+    };
+  });
 
   function registerEvent(node, eventName, callback) {
     var eventCache = getData(node, "_cashEvents") || setData(node, "_cashEvents", {});
