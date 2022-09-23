699d2c5043a04d432dd229651d887c9ffd2d2305
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2022-09-23 15:27:40 +0000

    cart template - card image style amended

--- the_coffee_co/settings.py
+++ the_coffee_co/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = ['thecoffeeco.herokuapp.com', 'localhost']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 
 INSTALLED_APPS = [
     'django.contrib.admin',
